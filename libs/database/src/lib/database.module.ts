5b6e994dded884645f484e3b7eb26195f4879d5e
Sompop Kulapalanont <sompo.ku@castcle.com>
2021-07-19 17:59:48 +0700

    add import db

--- libs/database/src/lib/database.module.ts
+++ libs/database/src/lib/database.module.ts
@@ -1,7 +1,10 @@
 import { Module, Global } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment} from "../environments/environment"
 
 @Global()
 @Module({
+  imports:[MongooseModule.forRoot(environment.dbLocation)],
   controllers: [],
   providers: [],
   exports: [],
