ee31db45b2187bff01842ed4b03a0d77bb09e54c
Sompop Kulapalanont <sompo.ku@castcle.com>
2021-07-20 13:27:28 +0700

    add account authen id schema

---
+++ libs/database/src/lib/schemas/accountAuthenId.schema.ts
@@ -0,0 +1,55 @@
+/* 
+ * Copyright (c) 2021, Castcle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *  
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 only, as
+ * published by the Free Software Foundation.
+ *  
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * version 3 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *  
+ * You should have received a copy of the GNU General Public License version
+ * 3 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *  
+ * Please contact Castcle, 22 Phet Kasem 47/2 Alley, Bang Khae, Bangkok,
+ * Thailand 10160, or visit www.castcle.com if you need additional information
+ * or have any questions.
+ */
+
+
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
+import { Document } from 'mongoose';
+import { Account } from "../schemas/account.schema"
+
+export type AccountAuthenIdDocument = AccountAuthenId & Document
+
+export enum AccountAuthenIdType{
+    Twitter = "twitter",
+    Facebook = "facebook",
+    Google = "google",
+    Telegram = "telegram"
+}
+
+@Schema()
+export class AccountAuthenId{
+
+    @Prop({required: true , type: mongoose.Schema.Types.ObjectId, ref:'Account'})
+    account:Account
+
+    @Prop({required:true})
+    type:string
+
+    @Prop({required:true})
+    createDate:Date
+
+    @Prop({required:true})
+    updateDate:Date
+}
+
+export const AccountAuthenIdSchema = SchemaFactory.createForClass(AccountAuthenId);
