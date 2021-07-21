ea3aa8edb9b723432879d360798f8ffa4d997068
Sompop Kulapalanont <sompo.ku@castcle.com>
2021-07-21 10:16:23 +0700

    add test example code

--- libs/database/src/lib/database.module.ts
+++ libs/database/src/lib/database.module.ts
@@ -1,6 +1,10 @@
-import { Module } from '@nestjs/common';
+import { Module, Global } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { environment} from "../environments/environment"
 
+@Global()
 @Module({
+  imports:[MongooseModule.forRoot(environment.dbLocation)],
   controllers: [],
   providers: [],
   exports: [],
