446490909d40d5489e6d0e2d8ba6d79025243e76
PopCastcle <87292763+PopCastcle@users.noreply.github.com>
2021-09-14 16:56:58 +0700

    init casl

--- libs/casl/src/lib/abilities/user.abilities.factory.ts
+++ libs/casl/src/lib/abilities/user.abilities.factory.ts
@@ -1,46 +1,38 @@
-/*
+/* 
  * Copyright (c) 2021, Castcle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
- *
+ *  
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 3 only, as
  * published by the Free Software Foundation.
- *
+ *  
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
  * FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
  * version 3 for more details (a copy is included in the LICENSE file that
  * accompanied this code).
- *
+ *  
  * You should have received a copy of the GNU General Public License version
  * 3 along with this work; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
- *
+ *  
  * Please contact Castcle, 22 Phet Kasem 47/2 Alley, Bang Khae, Bangkok,
  * Thailand 10160, or visit www.castcle.com if you need additional information
  * or have any questions.
  */
+import { UserDocument, ContentDocument } from '@castcle-api/database/schemas'
+import { InferSubjects } from '@casl/ability'
+import { User } from 'libs/database/src/lib/schemas/user.schema';
+import { Content } from 'libs/database/src/lib/schemas/content.schema';
+import { Injectable } from '@nestjs/common';
 
-module.exports = {
-  projects: [
-    '<rootDir>/apps/metadata',
-    '<rootDir>/apps/authentications',
-    '<rootDir>/apps/users',
-    '<rootDir>/apps/bases',
-    '<rootDir>/libs/commonDate',
-    '<rootDir>/libs/environments',
-    '<rootDir>/libs/database',
-    '<rootDir>/libs/logger',
-    '<rootDir>/libs/utils',
-    '<rootDir>/libs/utils/interceptors',
-    '<rootDir>/libs/utils/exception',
-    '<rootDir>/libs/utils/aws',
-    '<rootDir>/libs/utils/pipes',
-    '<rootDir>/apps/contents',
-    '<rootDir>/apps/engagements',
-    '<rootDir>/apps/backgrounds',
-    '<rootDir>/libs/utils/cache',
-    '<rootDir>/libs/ranker',
-    '<rootDir>/libs/casl'
-  ]
-};
+type Subjects = InferSubjects<typeof User | typeof Content> | 'all';
+
+enum Actions{
+  Manage = "manage",
+  Create = "create",
+  Read = "read",
+  Update = "update",
+  Delete = "delete",
+  Engage = "engage",
+}
