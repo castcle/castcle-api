4bcb325cd4161fb3b2f71c8909cfcb02e1928782
PopCastcle <87292763+PopCastcle@users.noreply.github.com>
2021-10-11 14:13:39 +0700

    remove unused interceptor

--- libs/database/src/lib/schemas/user.schema.ts
+++ libs/database/src/lib/schemas/user.schema.ts
@@ -27,10 +27,13 @@ import { Document, Model } from 'mongoose';
 import { SearchFollowsResponseDto } from '../dtos';
 import { EntityVisibility } from '../dtos/common.dto';
 import { PageResponseDto, UserResponseDto } from '../dtos/user.dto';
+import { Author } from '../dtos/content.dto';
 import { Account } from '../schemas/account.schema';
 import { CastcleBase } from './base.schema';
 import { RelationshipDocument } from './relationship.schema';
 import { Image } from '@castcle-api/utils/aws';
+import { CommentDocument, ContentDocument } from '.';
+import { FeedItemDocument } from './feedItem.schema';
 
 export type UserDocument = User & IUser;
 
@@ -119,10 +122,12 @@ export interface IUser extends Document {
   unfollow(user: UserDocument): Promise<void>;
   toSearchTopTrendResponse(): SearchFollowsResponseDto;
   toSearchResponse(): SearchFollowsResponseDto;
+  toAuthor(): Author;
 }
 
 export interface UserModel extends mongoose.Model<UserDocument> {
   covertToUserResponse(user: User | UserDocument): UserResponseDto;
+  toAuthor(user: User | UserDocument): Author;
 }
 
 const _covertToUserResponse = (self: User | UserDocument) => {
@@ -160,6 +165,20 @@ const _covertToUserResponse = (self: User | UserDocument) => {
 
 UserSchema.statics.covertToUserResponse = (self: User | UserDocument) =>
   _covertToUserResponse(self);
+
+UserSchema.statics.toAuthor = (self: User | UserDocument) =>
+  ({
+    id: self._id,
+    avatar:
+      self.profile && self.profile.images && self.profile.images.avatar
+        ? Image.download(self.profile.images.avatar)
+        : 'https://castcle-public.s3.amazonaws.com/assets/avatar-placeholder.png',
+    castcleId: self.displayId,
+    displayName: self.displayName,
+    followed: false, //default of followed
+    type: self.type,
+    verified: self.verified
+  } as Author);
 
 UserSchema.methods.toUserResponse = async function () {
   const self = await (this as UserDocument)
@@ -251,6 +270,9 @@ UserSchema.methods.toSearchResponse = function () {
 
 export const UserSchemaFactory = (
   relationshipModel: Model<RelationshipDocument>
+  /*contentModel: Model<ContentDocument>,
+  feedModel: Model<FeedItemDocument>,
+  commentModel: Model<CommentDocument>*/
 ): mongoose.Schema<any> => {
   /**
    * Make sure all aggregate counter is 0
@@ -272,6 +294,23 @@ export const UserSchemaFactory = (
       } as UserVerified;
     next();
   });
+
+  UserSchema.methods.toAuthor = function () {
+    const self = this as UserDocument;
+    return {
+      id: self._id,
+      avatar:
+        self.profile && self.profile.images && self.profile.images.avatar
+          ? Image.download(self.profile.images.avatar)
+          : 'https://castcle-public.s3.amazonaws.com/assets/avatar-placeholder.png',
+      castcleId: self.displayId,
+      displayName: self.displayName,
+      followed: false, //default of followed
+      type: self.type,
+      verified: self.verified
+    } as Author;
+  };
+
   UserSchema.methods.follow = async function (followedUser: UserDocument) {
     const session = await relationshipModel.startSession();
     await session.withTransaction(async () => {
