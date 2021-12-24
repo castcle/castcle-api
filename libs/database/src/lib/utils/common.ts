0dee2694f3e962f5abfdf5859b9b833afcaa572c
PopCastcle <87292763+PopCastcle@users.noreply.github.com>
2021-12-24 17:34:01 +0700

    hot fix metadata

--- libs/database/src/lib/utils/common.ts
+++ libs/database/src/lib/utils/common.ts
@@ -58,11 +58,11 @@ export const createCastcleMeta = (documents: Document[]): CastcleMeta => {
     const firstDocDate = documents[0]['createdAt'] as Date;
     const lastDocDate = documents[documents.length - 1]['createdAt'];
     if (firstDocDate.getTime() > lastDocDate.getTime()) {
-      meta.oldestId = documents[documents.length - 1].id;
-      meta.newestId = documents[0].id;
-    } else {
       meta.oldestId = documents[0].id;
       meta.newestId = documents[documents.length - 1].id;
+    } else {
+      meta.oldestId = documents[documents.length - 1].id;
+      meta.newestId = documents[0].id;
     }
   }
   meta.resultCount = documents.length;
