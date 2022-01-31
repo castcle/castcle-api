70929a55c8225f39c1607676d73cfff3db1feeff
kittipong-castcle <kittipong.ma@castcle.com>
2022-01-31 12:31:25 +0700

    Fix JSON.parse with undefined error in cache interceptor

--- libs/utils/interceptors/src/lib/cache/http.cache.clear.intercepter.ts
+++ libs/utils/interceptors/src/lib/cache/http.cache.clear.intercepter.ts
@@ -31,29 +31,32 @@ import {
   NestInterceptor,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import * as util from '../util';
+import { Cache } from 'cache-manager';
+import { getTokenFromRequest } from '../util';
 
 @Injectable()
 export class HttpCacheClearInterceptor implements NestInterceptor {
   constructor(
-    @Inject(CACHE_MANAGER) private cacheManager,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
     private reflector: Reflector
   ) {}
+
   async intercept(context: ExecutionContext, next: CallHandler<any>) {
-    //throw new Error('Method not implemented.');
     const cacheKey = this.reflector.get(
       CACHE_KEY_METADATA,
       context.getHandler()
     );
+
     if (cacheKey) {
       const request = context.switchToHttp().getRequest();
-      const token = util.getTokenFromRequest(request);
-      const setting: { [key: string]: any } =
-        JSON.parse(await this.cacheManager.get(token)) || {};
-      await Promise.all(
-        Object.keys(setting).map((delKey) => this.cacheManager.del(delKey))
-      );
-      await this.cacheManager.del(token);
+      const token = getTokenFromRequest(request);
+      const settingsString = await this.cacheManager.get<string>(token);
+      const settings = settingsString ? JSON.parse(settingsString) : {};
+
+      await Promise.all([
+        ...Object.keys(settings).map((delKey) => this.cacheManager.del(delKey)),
+        this.cacheManager.del(token),
+      ]);
     }
 
     return next.handle();
