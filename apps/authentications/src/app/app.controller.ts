f07a2c4a3221a261ccba841c9da5dfb671ebfafe
Zong <piya.ch@castcle.com>
2022-02-16 19:52:28 +0700

    block guest account in connect-with-social  api

--- apps/authentications/src/app/app.controller.ts
+++ apps/authentications/src/app/app.controller.ts
@@ -761,6 +761,12 @@ export class AuthenticationController {
   ) {
     this.logger.log(`connect with social: ${body.provider}`);
     this.logger.log(`payload: ${JSON.stringify(body)}`);
+
+    const currentAccount = await this.authService.getAccountFromCredential(
+      req.$credential
+    );
+    if (currentAccount?.isGuest) throw CastcleException.FORBIDDEN;
+
     const socialAccount = await this.authService.getAccountAuthenIdFromSocialId(
       body.socialId,
       body.provider
@@ -769,10 +775,6 @@ export class AuthenticationController {
       this.logger.error(`already connect social: ${body.provider}.`);
       throw new CastcleException(CastcleStatus.SOCIAL_PROVIDER_IS_EXIST);
     }
-
-    const currentAccount = await this.authService.getAccountFromCredential(
-      req.$credential
-    );
 
     this.logger.log(`connect account with social`);
     await this.authService.createAccountAuthenId(
