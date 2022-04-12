9823f8f1a53cd050c83408711ebe487a014a84a1
Evenajah <tharadol.bo@castcle.com>
2022-04-12 12:37:40 +0700

    api email exists
    fixed issue pr
    
    refractor api exists email
    
    move logic to service existEmail
    
    fix issue code style
    
    fix issue pull request
    
    optional castcleinclude type

--- libs/database/src/lib/services/authentication.service.v2.spec.ts
+++ libs/database/src/lib/services/authentication.service.v2.spec.ts
@@ -4,20 +4,19 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MongoMemoryServer } from 'mongodb-memory-server';
 import {
+  AuthenticationService,
   AuthenticationServiceV2,
   MongooseAsyncFeatures,
   MongooseForFeatures,
 } from '../database.module';
 import { EntityVisibility } from '../dtos';
-import { QueueName } from '../models';
 import { Account, AccountActivation, Credential } from '../schemas';
-import {
-  AuthenticationService,
-  SignupRequirements,
-} from './authentication.service';
+import { SignupRequirements } from './authentication.service';
+import { QueueName } from '../models';
 import { ContentService } from './content.service';
 import { HashtagService } from './hashtag.service';
 import { UserService } from './user.service';
+
 describe('Authentication Service', () => {
   let mongod: MongoMemoryServer;
   let app: TestingModule;
@@ -35,8 +34,8 @@ describe('Authentication Service', () => {
         MongooseForFeatures,
       ],
       providers: [
-        AuthenticationService,
         AuthenticationServiceV2,
+        AuthenticationService,
         UserService,
         ContentService,
         HashtagService,
@@ -56,13 +55,19 @@ describe('Authentication Service', () => {
     userService = app.get(UserService);
   });
 
-  describe('#getExistedUserFromCastcleId', () => {
+  describe('#Exist', () => {
     let signupResult: AccountActivation;
     let createAccountResult: {
       accountDocument: Account;
       credentialDocument: Credential;
     };
     const newDeviceUUID = '83b696d7-320b-4402-a412-d9cee10fc6a3';
+    const signupRequirements: SignupRequirements = {
+      displayId: 'people',
+      displayName: 'People',
+      email: 'sompopdude@dudedude.com',
+      password: '2@HelloWorld',
+    };
 
     beforeAll(async () => {
       createAccountResult = await serviceV1.createAccount({
@@ -73,14 +78,7 @@ describe('Authentication Service', () => {
           platform: 'iOs',
         },
       });
-    });
-    const signupRequirements: SignupRequirements = {
-      displayId: 'people',
-      displayName: 'People',
-      email: 'sompopdude@dudedude.com',
-      password: '2@HelloWorld',
-    };
-    beforeAll(async () => {
+
       signupResult = await serviceV1.signupByEmail(
         createAccountResult.accountDocument,
         {
@@ -91,34 +89,62 @@ describe('Authentication Service', () => {
         }
       );
     });
-    it('should create an accountActivation', () => {
-      expect(signupResult).toBeDefined();
-    });
-    it('should return exist user is null', async () => {
-      const id = 'undefined';
-      const findUser = await service.getExistedUserFromCastcleId(id);
-      expect(findUser).toBeNull();
-    });
-    it('should return exist user not null', async () => {
-      const id = 'people';
-      const findUser = await service.getExistedUserFromCastcleId(id);
-      expect(findUser).not.toBeNull();
-    });
-    it('should set statuses of user all to deleted', async () => {
-      const id = 'people';
-      const findUser = await service.getExistedUserFromCastcleId(id);
-      findUser.visibility = EntityVisibility.Deleted;
-      expect(findUser.visibility).toEqual(EntityVisibility.Deleted);
-    });
-  });
 
-  afterAll(async () => {
-    await app.close();
-    await mongod.stop();
-  });
+    describe('#getExistedUserFromCastcleId()', () => {
+      it('should create an accountActivation', () => {
+        expect(signupResult).toBeDefined();
+      });
+      it('should return exist user is null', async () => {
+        const id = 'undefined';
+        const findUser = await service.getExistedUserFromCastcleId(id);
+        expect(findUser).toBeNull();
+      });
+      it('should return exist user not null', async () => {
+        const id = 'people';
+        const findUser = await service.getExistedUserFromCastcleId(id);
+        expect(findUser).not.toBeNull();
+      });
+      it('should set statuses of user all to deleted', async () => {
+        const id = 'people';
+        const findUser = await service.getExistedUserFromCastcleId(id);
+        findUser.visibility = EntityVisibility.Deleted;
+        expect(findUser.visibility).toEqual(EntityVisibility.Deleted);
+      });
+    });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
-    expect(userService).toBeDefined();
+    describe('#getAccountFromEmail()', () => {
+      it('should return null for non exist email in account', async () => {
+        const testEmail = 'yotest@gmail.com';
+        const result = await service.getAccountFromEmail(testEmail);
+        expect(result).toBeNull();
+      });
+      it('should found an account that have email match', async () => {
+        const newlyInsertEmail = `${Math.ceil(
+          Math.random() * 1000
+        )}@testinsert.com`;
+        const newAccount = new (service as any).accountModel({
+          email: newlyInsertEmail,
+          password: 'sompop2@Hello',
+          isGuest: true,
+          preferences: {
+            languages: ['en', 'en'],
+          },
+        });
+        const newAccountResult = await newAccount.save();
+        const result = await service.getAccountFromEmail(newlyInsertEmail);
+        expect(result._id).toEqual(newAccountResult._id);
+      });
+    });
+
+    afterAll(async () => {
+      await app.close();
+      await mongod.stop();
+    });
+
+    it('should be defined', () => {
+      expect(service).toBeDefined();
+      expect(serviceV1).toBeDefined();
+      expect(userService).toBeDefined();
+    });
   });
 });
