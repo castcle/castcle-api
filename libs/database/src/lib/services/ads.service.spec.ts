a91118e3a7afb4cbc85b8400d1e10a77ffb38cb9
PopCastcle <87292763+PopCastcle@users.noreply.github.com>
2022-06-06 11:56:22 +0700

    Update AdsService (#1297)
    * refactor repo
    
    * add update ads status queue
    
    * update lint/ photo
    
    update test
    
    update test http module
    
    fix lint order
    
    fix lint bug
    
    * update new eslint
    
    * add script ads
    
    * fix lint 2
    
    * fix merge
    
    * merge fix
    
    Co-authored-by: popcastcle <sompop.k@castcle.com>

--- libs/database/src/lib/services/ads.service.spec.ts
+++ libs/database/src/lib/services/ads.service.spec.ts
@@ -20,7 +20,7 @@
  * Thailand 10160, or visit www.castcle.com if you need additional information
  * or have any questions.
  */
-
+import { HttpModule } from '@nestjs/axios';
 import { getQueueToken } from '@nestjs/bull';
 import { CacheModule } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -45,6 +45,7 @@ import {
   QueueName,
   WalletType,
 } from '../models';
+import { Repository } from '../repositories';
 import { AdsCampaign, Content } from '../schemas';
 import { AdsService } from './ads.service';
 import { AuthenticationService } from './authentication.service';
@@ -67,6 +68,7 @@ describe('AdsService', () => {
     app = await Test.createTestingModule({
       imports: [
         CacheModule.register(),
+        HttpModule,
         MongooseModule.forRoot(mongod.getUri()),
         MongooseAsyncFeatures,
         MongooseForFeatures,
@@ -78,6 +80,7 @@ describe('AdsService', () => {
         ContentService,
         TAccountService,
         HashtagService,
+        Repository,
         {
           provide: DataService,
           useValue: {
