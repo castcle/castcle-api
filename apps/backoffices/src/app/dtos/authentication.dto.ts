9f7baad6cf05d2d78c67445629a009b9d766a481
dfs <tharadol.b0@castcle.com>
2022-06-21 11:22:01 +0700

    Update authentication.dto.ts

--- apps/backoffices/src/app/dtos/authentication.dto.ts
+++ apps/backoffices/src/app/dtos/authentication.dto.ts
@@ -15,12 +15,15 @@ export class AccountDto {
   @IsString()
   @IsNotEmpty()
   email: string;
+
   @IsString()
   @IsNotEmpty()
   password: string;
+
   @IsString()
   @IsNotEmpty()
   firstName: string;
+
   @IsString()
   @IsNotEmpty()
   lastName: string;
