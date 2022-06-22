f7c27aa4450e212aeb0a1e1e190f9eb1dc05fa7d
Tharadol <tharadol.bo@castcle.com>
2022-06-22 10:22:24 +0700

    Auto stash before cherry pick of "remove token file in backoffice"

--- libs/utils/commons/src/lib/token.ts
+++ libs/utils/commons/src/lib/token.ts
@@ -21,24 +21,34 @@
  * or have any questions.
  */
 
-import { sign, verify } from 'jsonwebtoken';
+import * as jwt from 'jsonwebtoken';
+const generateToken = (payload: any, secret: string, expireIn: number) =>
+  jwt.sign(payload, secret, {
+    expiresIn: expireIn,
+    header: {
+      alg: 'HS256',
+      typ: 'JWT',
+    },
+  });
 
-export const Token = {
-  generateToken: (payload: any, secret: string, expireIn: number) =>
-    sign(payload, secret, {
-      expiresIn: expireIn,
-      header: { alg: 'HS256', typ: 'JWT' },
-    }),
+const isTokenValid = (token: string, secret: string) => {
+  try {
+    jwt.verify(token, secret);
+    return true;
+  } catch (error) {
+    console.error('error', error);
+    return false;
+  }
+};
 
-  isTokenValid: (token: string, secret: string) => {
-    try {
-      verify(token, secret);
-      return true;
-    } catch (error) {
-      console.error('error', error);
-      return false;
-    }
-  },
+const isTokenExpire = (token: string, secret: string) => {
+  return new Promise<boolean>((resolve) => {
+    jwt.verify(token, secret, (error) => {
+      if (error && error.name === 'TokenExpiredError') {
+        resolve(true);
+      } else resolve(false);
+    });
+  });
 };
 
 const decodeToken = <T = any>(token: string) => {
