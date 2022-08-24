88ea504e2a887f4cc5d375c05639ce9a26248348
popcastcle <sompop.k@castcle.com>
2022-08-24 10:41:10 +0700

    update time

--- libs/database/src/lib/services/content.service.v2.ts
+++ libs/database/src/lib/services/content.service.v2.ts
@@ -1083,8 +1083,13 @@ export class ContentServiceV2 {
   };
 
   expireAllFarmedToken = async () => {
+    const cutOffDate = new Date(
+      new Date().getTime() -
+        Environment.CONTENT_FARMING_COOLDOWN_HR * 60 * 1000,
+    );
     const expiresFarmings = await this.contentFarmingModel.find({
       status: ContentFarmingStatus.Farming,
+      startAt: { $lte: cutOffDate },
     });
     return Promise.all(
       expiresFarmings.map((cf) =>
